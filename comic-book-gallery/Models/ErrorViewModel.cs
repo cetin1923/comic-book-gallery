c11d8e53d73c70cdac3027bcd61e77d3133761dd
cetinarslan <cetinarslan.se@gmail.com>
2022-03-29 17:05:47 +0200

    my second commit

---
+++ comic-book-gallery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace comic_book_gallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
