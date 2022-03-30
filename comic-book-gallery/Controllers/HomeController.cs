51449e367639ebc3a8d7d46023231ce6b6dcb4af
cetinarslan <cetinarslan.se@gmail.com>
2022-03-30 13:26:08 +0200

    Updated Comic Book Detail View

--- comic-book-gallery/Controllers/HomeController.cs
+++ comic-book-gallery/Controllers/HomeController.cs
@@ -10,32 +10,22 @@ using ComicBookGallery.Models;
 namespace ComicBookGallery.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    { 
 
         public ActionResult Detail()
         {
+
+         ViewBag.SeriesTitle = "The Amazing Spider-Man";
+         ViewBag.IssueNumber = 700;
+         ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+         ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
             return View();
         }
     }
