2f28de73051be77489706cfdf45c7dd0d7aeb8ad
cetinarslan <cetinarslan.se@gmail.com>
2022-03-31 17:04:40 +0200

    changed the controller name to ComicBooks

---
+++ comic-book-gallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using comic_book_gallery.Models;
+using comic_book_gallery.Data;
+using System;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+               // return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
+            
+            return View(comicBook);
+        }
+
+        
+    }
+}
+
